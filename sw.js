577092811b07316a1e37fe469641c0e022539976
李燕 <liyan@MacBook-Pro-4.local>
2026-08-13 18:40:37 +0800

    Reject incomplete score cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gold-signal-shell-v1';
+const CACHE = 'gold-signal-shell-v3';
 const SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', event => {
